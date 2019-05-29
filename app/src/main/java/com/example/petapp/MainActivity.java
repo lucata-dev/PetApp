7cad8b6a0538fc4487d0e03cea646ad3a246a429
Lucas <lucasgutierrez85@gmail.com>
2019-05-28 21:59:31 -0300

    Home Activity

---
+++ app/src/main/java/com/example/petapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.petapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnGoogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGoogle = (Button) findViewById(R.id.btnGoogle);
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Implementar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
